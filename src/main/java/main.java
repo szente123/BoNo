aa3eed72609ab9ac166b870e6e314260fef8d468
Szente123 <nszentesi@gmail.com>
2022-02-16 09:28:11 +0100

    Osszegzes

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,20 @@
 public class main {
     public static void main(String[] args) {
         vezerlo start = new vezerlo();
+        int[] tomb = {1,2,5,8,11,33};
+        int eredmény;
 
+        eredmény = osszegzésTetel(tomb);
+
+    }
+
+    private static int osszegzésTetel(int[] tomb) {
+        int vissza =0;
+        for (int Item:tomb) {
+            vissza += Item;
+
+        }
+        return vissza;
     }
 
 }
--- src/main/java/vezerlo.java
+++ src/main/java/vezerlo.java
@@ -1,5 +1,10 @@
 public class vezerlo {
     public vezerlo() {
         System.out.println("vezerlo.vezerlo");
+        int[] tomb = {1,2,5,8,11,33};
+        int eredmény;
+
+
+        
     }
 }
