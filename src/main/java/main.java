cda9b6a3820c0674f6faff66190900a17d979e69
Bozso69 <bozsoky.zoltan@bozsofalvi.hu>
2022-02-16 08:29:40 +0100

    Összegzés Tétele

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,17 @@
 public class main {
     public static void main(String[] args) {
-        vezerlo start = new vezerlo();
+        int[] tomb = {1, 2, 4, 5, 43, 6, 8, 10};
+        int eredmeny;
 
+        eredmeny = osszegzesTetele(tomb);
+
+    }
+    private static int osszegzesTetele(int[] tomb) {
+        int vissza = 0;
+        for (int Item: tomb) {
+            vissza += Item;
+        }
+        return vissza;
     }
 
 }
